1c308eaa9f72332ba8a42e24681692de05fd763f
Sharavanakumar35 <shark.35.kumar@gmail.com>
2024-01-02 19:03:38 +0530

    circleClass updated

--- circleClass.js
+++ circleClass.js
@@ -1,2 +1,46 @@
 class Circle {
+
+    constructor(radius=1.0, color="red"){
+        this._radius = radius;
+        this._color = color;
+    }
+
+    get radius() {
+        return this._radius;
+    }
+
+    set radius(radius) {
+        this._radius = radius;
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    set color(color) {
+        this._color = color;
+    }
+
+    toString() {
+        return `Circle[radius = ${this._radius}, color = ${this._color}]`;
+    }
+
+    getArea() {
+        return Math.PI*Math.pow(this._radius, 2);
+    }
+
+    getCircumference() {
+        return 2*Math.PI*this._radius;
+    }
 }
+
+const circle = new Circle;
+
+circle.color = "orange";
+circle.radius = "2.56";
+
+console.log('Radius = ', circle._radius);
+console.log('Color = ', circle._color);
+console.log('toString = ', circle.toString());
+console.log('Area of the circle = ', circle.getArea());
+console.log('Circumference of the circle = ', circle.getCircumference());
